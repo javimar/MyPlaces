1513e443c2fb2a9b5368cfa8875d85744c5ff355
JaviMar <javimardeveloper@gmail.com>
2019-05-15 13:37:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Places
+
+
+This handy app lets us save and capture our favorite places. We can store a pic, geolocation, and several other cool properties for our place.
+
+App is available in both Spanish and English.
---
+++ app/src/main/java/eu/javimar/myplaces/Lugar.kt
@@ -0,0 +1,14 @@
+package eu.javimar.myplaces
+
+import eu.javimar.myplaces.utils.GeoPunto
+
+data class Lugar(val nombre: String,
+                 var direccion: String = "",
+                 var posicion: GeoPunto = GeoPunto.SIN_POSICION,
+                 var foto: String = "",
+                 var telefono: Int = 0,
+                 var url: String= "",
+                 var comentarios: String = "",
+                 var fecha: Long = System.currentTimeMillis(),
+                 var valoracion: Float = 3.5F
+)
